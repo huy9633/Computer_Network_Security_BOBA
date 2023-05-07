269842f0322495cbe92994bf0beeb9396c8fd555
huy9633 <lamchanhuy9633@gmail.com>
2023-05-07 15:28:40 +0700

    Custome FE

--- BOBA/xss/index.php
+++ BOBA/xss/index.php
@@ -10,30 +10,65 @@ if (empty($_SESSION['contents'])) {
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>XSS Demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <style>
+        .bd-example {
+            padding: 1.5rem;
+            margin-right: 0;
+            margin-left: 0;
+            border-width: 1px;
+            border-top-left-radius: 0.25rem;
+            border-top-right-radius: 0.25rem;
+            border-style: solid;
+        }
+    </style>
 </head>
+
 <body>
-    <div class="container py-5">
-        <a href="<?= base_url() ?>">Home</a>
-        <hr>
-        <h2>Demo Form</h2>
-        <?php if (has_flash_message('msg')) : ?>
+    <header>
+        <nav class="navbar navbar-expand-sm navbar-toggleable-sm navbar-light bg-white border-bottom box-shadow mb-3">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="/BOBA">BOBA</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target=".navbar-collapse"
+                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="navbar-collapse collapse d-sm-inline-flex justify-content-between">
+                    <ul class="navbar-nav flex-grow-1">
+                        <li class="nav-item active">
+                            <a class="nav-link  " href="<?= base_url() ?>">Home</a>
+                        </li>
+                        <li class="nav-item active">
+                            <a class="nav-link  " href="/BOBA/csrf">CSRF</a>
+                        </li>
+                        <li class="nav-item active">
+                            <a class="nav-link active " href="/BOBA/xss">XSS</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <div class="container w-50 pb-3">
+        <h1 style="text-align: center">Demo Form</h1>
+        <?php if (has_flash_message('msg')): ?>
             <div class="alert alert-success" role="alert">
                 <?= flash_message('msg') ?>
             </div>
         <?php endif ?>
-        <?php if (has_flash_message('error')) : ?>
+        <?php if (has_flash_message('error')): ?>
             <div class="alert alert-danger" role="alert">
                 <?= flash_message('error') ?>
             </div>
         <?php endif ?>
-        <div class="row mb-3">
-            <div class="col-md-5 col-lg-4">
+        <div class="bd-example mb-3" style="margin:auto"> 
                 <form action="<?= base_url('xss/submit.php') ?>" method="POST">
                     <?= csrf_security()->generateInput() ?>
                     <div class="mb-3">
@@ -42,17 +77,21 @@ if (empty($_SESSION['contents'])) {
                     </div>
                     <button type="submit" class="btn btn-primary">Submit</button>
                     <a href="<?= base_url('xss/delete.php') ?>" class="btn btn-danger">Delete All</a>
-                </form>
-            </div>
+                </form> 
         </div>
         <ul class="list-group">
-            <?php foreach($_SESSION['contents'] as $content) : ?>
+            <?php foreach ($_SESSION['contents'] as $content): ?>
                 <li class="list-group-item">
-                    <?= $content ?>    
+                    <?= $content ?>
                     <!-- <?= XSSParser::get($content) ?> -->
                 </li>
             <?php endforeach ?>
         </ul>
     </div>
+    <footer class="border-top footer text-muted">
+        <p class="text-center text-muted">&copy; 2023 - BOBA</p>
+
+    </footer>
 </body>
+
 </html>
